7f6bdb0403ce9ca0d89bd660a6816e8c96b98c41
salini4kamal <salini4kaml@gmail.com>
2022-11-18 13:02:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,90 @@
+import express from "express"
+import cors from "cors"
+import dotenv from 'dotenv'
+import nodemailer from 'nodemailer'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+import { MongoClient } from 'mongodb';
+const app = express()
+app.use(express.json());
+dotenv.config()
+app.use(cors())
+
+var mongoUrl = process.env.mongoUrl;
+// var PORT=process.env.PORT || 4000;
+
+
+
+async function createConnection(){
+    var client = new MongoClient(mongoUrl);
+    await client.connect()
+    console.log("connection is ready ")
+ return client
+}
+export var client = await createConnection()
+
+async function passwordMatch(pass){
+    var salt = await bcrypt.genSalt(4);
+    var hash = await bcrypt.hash(pass,salt);
+    return hash;
+}
+app.get('/Menu',async(req,res)=>{
+    let result = await client.db("data").collection("pizza").find({}).toArray();
+    res.send(result)
+})
+
+app.post("/signin", async function(req,res){
+    let {email,password} = req.body
+    let hash = await passwordMatch(password)
+    let result = await client.db("product").collection("users").insertOne({email,"password":hash})
+    res.send(result)
+})
+app.post("/login",async function(req,res){
+    let {email,password}=req.body;
+    let result =await client.db("product").collection("users")
+    .findOne({email});
+    if(!result){
+        res.status(401).send({msg:"invalid"})
+    }else{
+        var storedPassword = result.password
+        var compare = await bcrypt.compare(password,storedPassword)
+        if(!compare){
+            res.status(401).send({msg:"invalid"})
+        }else{
+            const token = await jwt.sign({id:result._id},"salini");
+            async function nodemail(){
+                var transfer = nodemailer.createTransport({
+                    service:"hotmail",
+                    auth:{
+                       user:"salini4kamal@gmail.com",
+                       pass:"salini1234"
+                    }
+                 
+                 })
+                   const options={
+                    from:"salini4kamal@gmail.com",
+                    to:email,
+                    subject:"your login",
+                    text:"your login into pizz shop"
+                   }
+                 
+                   transfer.sendMail(options,(err)=>{
+                    if(err){
+                       console.log(err)
+                    }else{
+                       console.log({msg:"mail send"})
+                    }
+                   })
+                   transfer.verify()
+                }
+                nodemail()
+                res.send({msg:"login sucessfully",token:token})
+        }
+    }
+  })
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log("server is ready")
+});
